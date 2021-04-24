ba087ad6e2da5c9c06c751c646aab9375e7ab754
EatBugz <77813942+EatBugz@users.noreply.github.com>
2021-04-23 19:17:38 -0700

    Added Sector 1 Mechanics. Check in Scripts

---
+++ OGPC Project/Assets/Scripts/Sector 1 Mechanics/LumenLight.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LumenLight : MonoBehaviour
+{
+    // important variables \\
+    public Transform player;
+    public Light lightThing;
+    public float intensityAmount = 5;
+    public int distanceBeforeLight = 9;
+
+    // getting the light component \\
+    void Start() {
+        lightThing = GetComponent<Light>();
+    }
+
+    // gets the distance from the lumen light to the player
+    // once gotten the distance, change the intensity of the light depending on the distance
+    void Update()
+    {
+        // get the distance
+        float distance = Vector3.Distance(gameObject.transform.position, player.transform.position);
+
+        // change the intensity
+        if (distance < distanceBeforeLight) {
+            lightThing.intensity = intensityAmount - distance;
+        }
+        else {
+            lightThing.intensity = 0f;
+        }
+    }
+}
---
+++ OGPC Project/Assets/Scripts/Sector 1 Mechanics/goop.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goop : MonoBehaviour
+{
+    // the player's mass will be when interacting with the goop
+    public float massInGoop = 1.75f;
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+            rb.mass = massInGoop;
+        }
+    }
+
+    // reverting the player mass back when they leave the goop
+    void OnTriggerExit(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+            rb.mass = 1;
+        }
+    }
+} 
