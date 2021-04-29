b359907f4d22ad357434ae99d11cd7106d6303db
EatBugz <77813942+EatBugz@users.noreply.github.com>
2021-04-29 14:29:09 -0700

    Added Sector 2 Mechanics. Check in Scripts

---
+++ OGPC Project/Assets/Scripts/Sector 2 Mechanics/sleek.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sleek : MonoBehaviour
+{
+    // what the player's mass will be while in the sleek \\
+    public float massInSleek = 0.75f;
+
+    // checking if the player is in the sleek \\
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+            rb.mass = massInSleek;
+        }
+    }
+
+    // reverting changes once the player leaves the sleek \\
+    void OnTriggerExit(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+            rb.mass = 1;
+        }
+    }
+}
