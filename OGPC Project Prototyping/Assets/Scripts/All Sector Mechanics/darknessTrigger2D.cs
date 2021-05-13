385d44cd4ed306d340f15fb812bcc11b263e3292
EatBugz <77813942+EatBugz@users.noreply.github.com>
2021-05-13 14:09:33 -0700

    made changes to increase fps

--- OGPC Project Prototyping/Assets/Scripts/All Sector Mechanics/darknessTrigger2D.cs
+++ OGPC Project Prototyping/Assets/Scripts/All Sector Mechanics/darknessTrigger2D.cs
@@ -6,6 +6,7 @@ public class darknessTrigger2D : MonoBehaviour
 {
     public bool turnDark = true; // true if the trigger turns the environment dark, false if the opposite
     public GameObject manager;
+    public static bool dark = false;
 
     void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "Player") {
@@ -13,18 +14,22 @@ public class darknessTrigger2D : MonoBehaviour
             if (turnDark) {
                 if (other.gameObject.transform.position.x < gameObject.transform.position.x) {
                     StartCoroutine(manager.GetComponent<darknessManager>().fadeToDark());
+                    dark = true;
                 }    
                 else if (other.gameObject.transform.position.x > gameObject.transform.position.x) {
                     StartCoroutine(manager.GetComponent<darknessManager>().fadeToLight());
+                    dark = false;
                 } 
             }
             // if the trigger turns the setting light \\
             else {
                 if (other.gameObject.transform.position.x > gameObject.transform.position.x) {
                     StartCoroutine(manager.GetComponent<darknessManager>().fadeToDark());
+                    dark = true;
                 }    
                 else if (other.gameObject.transform.position.x < gameObject.transform.position.x) {
                     StartCoroutine(manager.GetComponent<darknessManager>().fadeToLight());
+                    dark = false;
                 } 
             }
         }
