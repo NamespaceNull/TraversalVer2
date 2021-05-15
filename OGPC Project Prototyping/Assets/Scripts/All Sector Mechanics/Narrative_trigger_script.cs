e16b8e70193bfd5b13df6a64c5b44aadfc92f527
Karen Page <karennicepage@gmail.com>
2021-05-14 17:45:41 -0700

    narrative trigger fixes (+text resizing on settings and back buttons)

--- OGPC Project Prototyping/Assets/Scripts/All Sector Mechanics/Narrative_trigger_script.cs
+++ OGPC Project Prototyping/Assets/Scripts/All Sector Mechanics/Narrative_trigger_script.cs
@@ -5,126 +5,19 @@ using UnityEngine;
 
 public class Narrative_trigger_script : MonoBehaviour
 {
-    private GameObject player;
-
-    public bool fromAbove;
-    public bool fromBelow;
-    public bool fromTheLeft;
-    public bool fromTheRight;
-
-    private int directionX;
-    private int directionY;
 
     // messages
-    public string topLeft;
-    public string topRight;
-    public string bottomLeft;
-    public string bottomRight;
-    public string top;
-    public string bottom;
-    public string left;
-    public string right;
-
-    // This will most likely be replaced with a reference to the isBroken map condition
-    [HideInInspector]
-    public bool broken = false;
-
-   // [HideInInspector]
-    public bool enter;
-
-    private int a;
-
-    void Start()
-    {
-     
-        player = GameObject.Find("Player");
-
-    }
+    bool dirIsX;
+    public string posMessage; // for if values are up or right
+    public string negMessage; // for if values are down or left
 
     public void GetTriggerAction(Vector2 vel)
     {
-        // start directions
-        fromAbove = false;
-        fromBelow = false;
-        fromTheRight = false;
-        fromTheLeft = false;
-        if (!broken)
-        {
-            // Set directions with values, and set enter based on whether the trigger worked
-            enter = true;
-            if (vel.y < 0) fromAbove = true;
-            else if (vel.y > 0) fromBelow = true;
-            else enter = false;
-            if (vel.x > 0)
-            {
-                fromTheLeft = true;
-                enter = true;
-            }
-            else if (vel.x < 0)
-            {
-                fromTheRight = true;
-                enter = true;
-
-            }
-
-            if (enter)
-            {
-                // Set text
-                if (fromTheLeft)
-                {
-                    if (fromAbove)
-                    {
-                        TextMeshPro componentTMP = GetComponent<TextMeshPro>();
-                        componentTMP.SetText(topLeft);
-
-                    }
-                    else if (fromBelow)
-                    {
-                        TextMeshPro componentTMP = GetComponent<TextMeshPro>();
-                        componentTMP.SetText(bottomLeft);
-                    }
-                    else
-                    {
-                        TextMeshPro componentTMP = GetComponent<TextMeshPro>();
-                        componentTMP.SetText(left);
-                    }
-                }
-                else if (fromTheRight)
-                {
-                    if (fromAbove)
-                    {
-                        TextMeshPro componentTMP = GetComponent<TextMeshPro>();
-                        componentTMP.SetText(topRight);
-                    }
-                    else if (fromBelow)
-                    {
-                        TextMeshPro componentTMP = GetComponent<TextMeshPro>();
-                        componentTMP.SetText(bottomRight);
-                    }
-                    else
-                    {
-                        TextMeshPro componentTMP = GetComponent<TextMeshPro>();
-                        componentTMP.SetText(right);
-                    }
-                }
-                else if (fromAbove)
-                {
-                    TextMeshPro componentTMP = GetComponent<TextMeshPro>();
-                    componentTMP.SetText(top);
-                }
-                else if (fromBelow)
-                {
-                    TextMeshPro componentTMP = GetComponent<TextMeshPro>();
-                    componentTMP.SetText(bottom);
-                }
-            }
-        }
+        string message;
+        float val = dirIsX ? vel.x : vel.y;
+        if (val < 0) message = posMessage;
+        else if (val > 0) message = negMessage;
+        else message = "";
+        GetComponent<TextMeshPro>().text = message;
     }
-
-    void OnTriggerExit(Collider col)
-    {
-        enter = false;
-    }
-
-    
 }
